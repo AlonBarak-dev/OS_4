59832c2c77b1efe70fb8c8522857c58444698666
AlonBarak-dev <alonbarak3110@gmail.com>
2022-04-12 23:36:51 +0300

    changed the stack from class in cpp to struct in c

--- stack.cpp
+++ stack.cpp
@@ -1,34 +1,89 @@
 #include "stack.hpp"
-#include <vector>
-#include <string>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-using namespace std;
-
-bool Stack::push(string str){
-    this->stck.push_back(str);
-    this->lastindex++;
-    this->size++;
-    return true;
-}
-
-void Stack::pop(){
-    try{
-        this->stck.pop_back();
-        this->lastindex--;
+void push(pnode *head, char data[1024]){
+    pnode new_node = (pnode) malloc(sizeof(node));      // dynamic allocation
+    new_node->data = data;
+    new_node->next = NULL;
+    
+    // in case the stack is empty -> the new node will be the head of the stack
+    if (*head == NULL)
+    {
+        *head = new_node;
     }
-    catch(exception &e){
-        throw runtime_error("Empty stack!");
-    }
-}
-
-string Stack::top(){
-    try{
-        string str = this->stck.at(this->lastindex);
-        return str;
-    }
-    catch(exception& e){
-        throw runtime_error("Empty stack!");
+    else
+    {
+        pnode *ptr = head;  //temporary pointer
+        while ((*ptr)->next)
+        {
+            // loop until the last node in the stack
+            ptr = &((*ptr)->next);      // increament the pointer
+        }
+        (*ptr)->next = new_node;     // add the new node to the stack
     }
 }
+
+void pop(pnode *head){
+
+    // remove the last node from the stack
+
+    if (*head == NULL){
+        // stack is empty, return while doing nothing
+        return;
+    }
+    pnode *ptr = head;  //temporary pointer
+    while ((*ptr)->next->next)
+    {
+        // loop until the one befre the last node in the stack
+        ptr = &((*ptr)->next);      // increament the pointer
+    }
+    pnode del = (*ptr)->next;    // the node we want to delete
+    (*ptr)->next = NULL;    // remove the last node from the stack
+
+    // free the deleted node
+    free(del); 
+}
+
+char* top(pnode head){
+
+    // return the data of the last node in the stack
+    // loop until the last node
+    while (head->next)
+    {
+        // increament the node
+        head = head->next;
+    }
+    return head->data;
+}
+
+
+// using namespace std;
+
+// bool Stack::push(string str){
+//     this->stck.push_back(str);
+//     this->lastindex++;
+//     this->size++;
+//     return true;
+// }
+
+// void Stack::pop(){
+//     try{
+//         this->stck.pop_back();
+//         this->lastindex--;
+//     }
+//     catch(exception &e){
+//         throw runtime_error("Empty stack!");
+//     }
+// }
+
+// string Stack::top(){
+//     try{
+//         string str = this->stck.at(this->lastindex);
+//         return str;
+//     }
+//     catch(exception& e){
+//         throw runtime_error("Empty stack!");
+//     }
+// }
--- stack.hpp
+++ stack.hpp
@@ -1,22 +1,34 @@
-#include <vector> 
-#include <iostream>
-#include <string>
+#ifndef STACK_
+#define STACK_
 
 
 
-
-class Stack
+typedef struct stack_node
 {
-private:
-    
-    int size;
-    int lastindex;
-    std::vector<std::string> stck;
+    char* data;
+    struct stack_node *next;
+} node, *pnode;
 
-public:
-    Stack() : size(0), lastindex(-1){}
-    ~Stack(){}
-    bool push(std::string str);
-    void pop();
-    std::string top();
-};
+void push(pnode *head, char data[1024]); // push -> receives head of stack (double pointer) & data array
+void pop(pnode *head);  // pop -> receives head of stack (double pointer)
+char* top(pnode head);  // top -> receives head of stack (pointer)
+
+#endif
+
+
+
+// class Stack
+// {
+// private:
+    
+//     int size;
+//     int lastindex;
+//     std::vector<std::string> stck;
+
+// public:
+//     Stack() : size(0), lastindex(-1){}
+//     ~Stack(){}
+//     bool push(std::string str);
+//     void pop();
+//     std::string top();
+// };
