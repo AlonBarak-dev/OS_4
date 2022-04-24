063a71327f73a4945c3b6a766ffffb21bf58c871
AlonBarak-dev <alonbarak3110@gmail.com>
2022-04-24 21:48:01 +0300

    added more tests

--- test.cpp
+++ test.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 void push(pnode *head, char data[1024]); // push -> receives head of stack (double pointer) & data array
 void pop(pnode *head);  // pop -> receives head of stack (double pointer)
@@ -11,6 +13,7 @@ char* top(pnode head);  // top -> receives head of stack (pointer)
 
 int main(){
 
+    // ~~~~~~~~~~~~~~~~~~~~~ TESTS FOR THE STACK STRUCT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     pnode st = (pnode)malloc(sizeof(node));     // inititalize a stack
     char buffer[1024] = {0};
     // push "hello" into ths stack
@@ -44,6 +47,110 @@ int main(){
 
     // free the stack
     free(st);
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~ DONE WITH STACK'S TESTS ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~ TESTS FOR THE WHOLE CLIENT-SERVER SYSTEM ~~~~~~~~~
+
+    /*
+        Please make sure that the server is running before those tests!
+    */
+
+    pid_t pid_first = fork(); // create a new process
+    if (pid_first < 0)
+    {
+        // fail
+        printf("Failed to create a new CLient");
+    }
+
+    if (pid_first == 0)
+    {
+        // child process
+        char cmd[] = "./client_test1";
+        char *child_args;
+        char * argv[] = {cmd, NULL};
+        // run the client
+        execvp(argv[0], argv);
+        exit(127);
+    }
+    else{
+        // parent process
+        sleep(2);
+        pid_t pid_second = fork(); // create a new process
+        if (pid_second < 0)
+        {
+            // fail
+            printf("Failed to create a new CLient");
+        }
+
+        if (pid_second == 0)
+        {
+            // child process
+            char cmd[] = "./client_test2";
+            char * argv[] = {cmd, NULL};
+            // run the client
+            execvp(argv[0], argv);
+            exit(127);
+        }
+        else{
+            // parent process
+            sleep(2);
+            pid_t pid_third = fork(); // create a new process
+            if (pid_third < 0)
+            {
+                // fail
+                printf("Failed to create a new CLient");
+            }
+
+            if (pid_third == 0)
+            {
+                // child process
+                char cmd[] = "./client_test3";
+                char * argv[] = {cmd, NULL};
+                // run the client
+                execvp(argv[0], argv);
+                exit(127);
+            }
+            else{
+                int status;
+                while (waitpid(pid_third, &status,0) == -1)
+                {
+                    if (errno != EINTR)
+                    {
+                        status = -1;
+                        break;
+                    }
+                }   
+            }   // third process
+
+            int status;
+            while (waitpid(pid_second, &status,0) == -1)
+            {
+                if (errno != EINTR)
+                {
+                    status = -1;
+                    break;
+                }
+            }   
+        }   // second process
+
+
+        int status;
+        while (waitpid(pid_first, &status,0) == -1)
+        {
+            if (errno != EINTR)
+            {
+                status = -1;
+                break;
+            }
+        }   
+
+    }   // first process
+    
+    
+
+
 
 
     return 0;
